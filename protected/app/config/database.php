5c859cb1a2d65e4ce0237d2f6919e608d60bdb5d
tran-dung-mulodo <tran.dung@mulodo.com>
2015-03-13 17:02:39 +0700

    Config database

--- protected/app/config/database.php
+++ protected/app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'dichvuuber',
+			'database'  => 'dongho',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
